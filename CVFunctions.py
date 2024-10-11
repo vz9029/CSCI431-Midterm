3fa878f368dd41cb6b2e1c97da67e8f218f7073a
Vicky Zhu <vz9029@rit.edu>
2024-10-10 20:06:32 -0400

    2D Folder with images, 3D Folders,Skeleton  CVFunctions.py and main.py

---
+++ CVFunctions.py
@@ -0,0 +1,5 @@
+import cv2
+
+
+def CVFunction (image_path):
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+import CVFunctions
+
+def main():
+    input_2D = 'input_2DImages'
+    output_3D = 'output_3DObj'
+
+if __name__ == '__main__':
+    main()
