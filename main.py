c260ad380182e0b6b611e8ecdb28af4cf30dd255
Vicky Zhu <vz9029@rit.edu>
2024-10-10 20:20:27 -0400

    Added 2 functions to main

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import CVFunctions
+import cv2
 
+
+def Image2D(input_2D):
+    pass
+
+def Object3D(output_3D):
+    pass
 def main():
     input_2D = 'input_2DImages'
     output_3D = 'output_3DObj'
